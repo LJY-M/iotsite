51aa375bbe38fc0d3f0ae75063757a3702cd9c5a
miaomiaomiao-LJY <48284713+miaomiaomiao-LJY@users.noreply.github.com>
2020-12-03 14:21:15 +0800

    Initial commit

---
+++ src/main/java/com/lot/iotsite/IotsiteApplication.java
@@ -0,0 +1,13 @@
+package com.lot.iotsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IotsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lot/iotsite/IotsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lot.iotsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
