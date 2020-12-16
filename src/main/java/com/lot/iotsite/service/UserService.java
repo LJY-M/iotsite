f60a772bd6ea90d9839842f4ffcd7f7b43ade3ef
huangxin <597934012@qq.com>
2020-12-16 14:42:30 +0800

    UserService已经修改

--- src/main/java/com/lot/iotsite/service/UserService.java
+++ src/main/java/com/lot/iotsite/service/UserService.java
@@ -4,14 +4,6 @@ import com.baomidou.mybatisplus.extension.service.IService;
 import com.lot.iotsite.domain.User;
 
 
-/**
- * <p>
- * 用户表 服务类
- * </p>
- *
- * @author isHuangXin
- * @since 2020-12-04
- */
 public interface UserService extends IService<User> {
 
     User getUserById(Long id);
